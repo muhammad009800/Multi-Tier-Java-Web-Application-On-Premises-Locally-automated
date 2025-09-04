83dcaa040cd6c205bc2baa7e3782e48002ce3ed4
muhammad reda <muhammad reda@gmail.com>
2025-09-04 09:53:05 +0300

    db test

--- db01.sh
+++ db01.sh
@@ -1,6 +1,11 @@
 set -e
+
+
 echo "==== Updating system ===="
 sudo yum update -y
+
+
+echo "==== install bash ===="
 if ! command -v bash &> /dev/null
 then
     echo "bash not found, installing..."
@@ -8,20 +13,50 @@ then
 else
     echo "bash already installed"
 fi
+
+
 echo "==== Installing required packages ===="
 sudo yum install epel-release -y
 Sudo yum install git mariadb-server -y
+
+
 echo "==== Starting and enabling MariaDB ===="
 sudo systemctl enable --now mariadb
+
+
 echo "==== Securing MariaDB ===="
 sudo bash /vagrant/secure_mysql.sh
+
+
 echo "==== Initializing Tomcat DB ===="
 sudo mysql -u root -padmin123 < /vagrant/init_db.sql
+
+
+echo "==== Cloning GitHub project ===="
+if [ -d "/home/vagrant/vprofile-project" ]; then
+    echo "Project exists, removing old copy..."
+    rm -rf /home/vagrant/vprofile-project
+fi
 sudo git clone -b main https://github.com/hkhcoder/vprofile-project.git
 cd vprofile-project
+
+
+echo "==== Importing DB backup ===="
 sudo mysql -u root -padmin123 accounts < src/main/resources/db_backup.sql
+
+
+echo "==== Verifying imported tables ===="
 sudo mysql -u root -padmin123 accounts <<EOF
 SHOW TABLES;
 EOF
-sudo systemctl enable --now mariadb
+
+
+echo "==== Restarting MariaDB ===="
+sudo systemctl restart mariadb
+
+
+echo "==== Configuring firewall ===="
 sudo bash /vagrant/en_firwall_db.sh
+
+echo "==== Provisioning DB DONE ✅😎👌 ===="
+
