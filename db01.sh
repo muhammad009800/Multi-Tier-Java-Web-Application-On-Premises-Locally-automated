958774803d524af6c9eee422f0e262358b9e265c
muhammad reda <muhammad reda@gmail.com>
2025-09-04 09:41:56 +0300

    init db

--- db01.sh
+++ db01.sh
@@ -1,3 +1,5 @@
+set -e
+echo "==== Updating system ===="
 sudo yum update -y
 if ! command -v bash &> /dev/null
 then
@@ -6,11 +8,15 @@ then
 else
     echo "bash already installed"
 fi
+echo "==== Installing required packages ===="
 sudo yum install epel-release -y
 Sudo yum install git mariadb-server -y
+echo "==== Starting and enabling MariaDB ===="
 sudo systemctl enable --now mariadb
+echo "==== Securing MariaDB ===="
 sudo bash /vagrant/secure_mysql.sh
-sudo mysql -u root -padmin123 < /vagrant/init-tomcatdb.sql
+echo "==== Initializing Tomcat DB ===="
+sudo mysql -u root -padmin123 < /vagrant/init_db.sql
 sudo git clone -b main https://github.com/hkhcoder/vprofile-project.git
 cd vprofile-project
 sudo mysql -u root -padmin123 accounts < src/main/resources/db_backup.sql
