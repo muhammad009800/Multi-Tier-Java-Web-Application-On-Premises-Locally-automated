776ff64ef358b29ef87f58b8635184e35be6fb67
muhammad reda <muhammad reda@gmail.com>
2025-09-08 13:18:47 +0300

    finish task 1 automated

--- maven.sh
+++
@@ -1,9 +0,0 @@
-set -e
-sudo apt update
-sudo apt upgrade -y
-sudo apt install git maven -y
-git clone -b main https://github.com/hkhcoder/vprofile-project.git
-cd vprofile-project
-mvn install
-cp target/*.war /vagrant/myapp.war
-echo "WAR copied and Tomcat deploy triggered ✅"
